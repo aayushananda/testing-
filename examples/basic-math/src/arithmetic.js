31a8ccc4919aa4097c821ba6d7cfed419969d491
aayushananda <aayushanand300@gmail.com>
2026-05-19 07:17:34 +0530

    Testing for invalid input

--- examples/basic-math/src/arithmetic.js
+++ examples/basic-math/src/arithmetic.js
@@ -1,4 +1,9 @@
 export const add = (a, b) => {
+  if (String(a) === a) a = Number(a);
+  if (typeof b === 'string') b = Number(b);
+
+  if (isNaN(a)) throw new Error('The first argument is not a number');
+  if (isNaN(b)) throw new Error('The second argument is not a number');
   return a + b;
 };
 
--- examples/basic-math/src/arithmetic.test.js
+++ examples/basic-math/src/arithmetic.test.js
@@ -5,6 +5,20 @@ describe('add', () => {
   it('should add two +ve number', () => {
     expect(add(2, 2)).toBe(4);
   });
+  it('should add two -ve numbers', () => {
+    expect(add(-2, -2)).toBe(-4);
+  });
+  it('should parse strings into numbers', () => {
+    expect(add(1, '1')).toBe(2);
+  });
+  it('should detect invalid input', () => {
+    expect(() => add(2, 'potato')).toThrow(
+      'The second argument is not a number',
+    );
+  });
+  it('should detect invalid input', () => {
+    expect(() => add('potato', 2)).toThrow('not a number');
+  });
 });
 
 describe.todo('subtract', () => {
