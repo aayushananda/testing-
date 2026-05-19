aefe8047bcb6d91a0650bc7406e5875d2d471869
aayushananda <aayushanand300@gmail.com>
2026-05-19 06:13:04 +0530

    Steve Kinney testing fundamentals

--- examples/basic-math/src/arithmetic.test.js
+++ examples/basic-math/src/arithmetic.test.js
@@ -1,9 +1,26 @@
 import { describe, it, expect } from 'vitest';
+import { add, subtract, multiply, divide } from './arithmetic.js';
 
-describe.todo('add', () => {});
+describe('add', () => {
+  it('should add two +ve number', () => {
+    expect(add(2, 2)).toBe(4);
+  });
+});
 
-describe.todo('subtract', () => {});
+describe.todo('subtract', () => {
+  it('should subtract two numbers', () => {
+    expect(subtract(5, 3)).toBe(2);
+  });
+});
 
-describe.todo('multiply', () => {});
+describe.todo('multiply', () => {
+  it('should multiply two numbers', () => {
+    expect(multiply(2, 4)).toBe(8);
+  });
+});
 
-describe.todo('divide', () => {});
+describe.todo('divide', () => {
+  it('should divide two numbers', () => {
+    expect(divide(9, 3)).toBe(3);
+  });
+});
